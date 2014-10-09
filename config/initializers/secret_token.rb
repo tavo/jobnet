98756835f9f77c189c4b21f7d9d1f9c48313475c
Gustavo Delgado <tavo.ucv@gmail.com>
2014-10-09 10:51:22 -0300

    initial commit.. rails new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Jobnet::Application.config.secret_key_base = 'c741eb6f0c81560b08791bfbebbb3b5482081b7d921df367f645030f8f225473a60256132fb921243b37066cb9b8d44ed75495562beb260db36f1bcc4da78889'
